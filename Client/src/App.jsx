0a2d00171e79558742933fbc7dfe26965bcd64c3
Abhigyan2005 <abhigyanjha334@gmail.com>
2025-03-08 20:54:22 +0530

    i hate vercel

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,16 @@ import Services from "./components/Services";
 import "./css/App.css";
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "../src/public/animation.js";
+    script.id = "animation-script";
+
+    if (!document.getElementById("animation-script")) {
+      document.body.append(script);
+    }
+  }, []);
+
   return (
     <>
       <Hero />
